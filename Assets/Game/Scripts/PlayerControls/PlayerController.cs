2e8f81c643db25a40180f82d360cae341671a6c1
Adriano Mazzucco <onairda97@Gmail.com>
2023-03-08 23:22:29 -0500

    moved tool to editor folder
    move the tool code to editor folder to that builds can be made

--- Assets/Game/Scripts/PlayerControls/PlayerController.cs
+++ Assets/Game/Scripts/PlayerControls/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         UserMovement();
         PlayerRotation();
