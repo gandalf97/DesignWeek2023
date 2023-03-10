36e1bd5ed5682c24419a0ab1f8659b370713c56d
Adriano Mazzucco <onairda97@Gmail.com>
2023-03-09 20:27:23 -0500

    added bowling assets

--- Assets/Game/Scripts/Environment/GroundTile.cs
+++ Assets/Game/Scripts/Environment/GroundTile.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(BoxCollider), typeof(MeshRenderer))]
 public class GroundTile : MonoBehaviour
 {
     
